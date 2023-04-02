222de9a0c1b5373106125635311aaa2173330588
35kartal35 <mozdemir3545@gmail.com>
2023-04-02 22:30:34 +0300

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+
+import Footer from './components/footer';
+import { BrowserRouter } from "react-router-dom";
+
+import Header from './components/header'
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <div className='container py-3'>
+
+        <Header />
+        burası ap
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+
+}
+
+export default App
